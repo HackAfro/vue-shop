b81fd4a467b30fb31a9e17a69824fe099e3cb7a8
Richard Afro <richyafro@hotmail.com>
2019-03-27 22:25:32 +0100

    Create products file

---
+++ products.js
@@ -0,0 +1,62 @@
+module.exports = [
+  {
+    "name": "Apples",
+    "price": 23.54,
+    "image": "https://images.pexels.com/photos/39028/apples-fruit-red-juicy-39028.jpeg?cs=srgb&dl=apples-food-fruits-39028.jpg&fm=jpg",
+    "description": "A big bite from an apple"
+  },
+  {
+    "name": "Lemons",
+    "price": 10.33,
+    "image": "https://images.pexels.com/photos/1898261/pexels-photo-1898261.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=400&w=400",
+    "description": "Great lemon slices"
+  },
+  {
+    "name": "Great dish",
+    "price": 12.13,
+    "image": "https://images.pexels.com/photos/37316/kaki-fruit-orange-subject.jpg?auto=compress&cs=tinysrgb&dpr=2&h=400&w=400",
+    "description": "Healthy meal served with a side of protein"
+  },
+  {
+    "name": "Mangoes",
+    "price": 10.33,
+    "image": "https://images.pexels.com/photos/918643/pexels-photo-918643.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=400&w=400",
+    "description": "A nice array of slushy mangoes."
+  },
+  {
+    "name": "Berries!",
+    "price": 10.33,
+    "image": "https://images.pexels.com/photos/1334131/pexels-photo-1334131.jpeg?cs=srgb&dl=abundance-berries-close-up-1334131.jpg&fm=jpg&auto=compress&cs=tinysrgb&dpr=2&h=400&w=400",
+    "description": "Berry abundance"
+  },
+  {
+    "name": "Coconut drink",
+    "price": 10.33,
+    "image": "https://images.pexels.com/photos/1030973/pexels-photo-1030973.jpeg?cs=srgb&dl=berries-berry-chia-1030973.jpg&fm=jpg&auto=compress&cs=tinysrgb&dpr=2&h=400&w=400",
+    "description": "Refreshing cocunut drink with strawberries"
+  },
+  {
+    "name": "Fruits",
+    "price": 16.43,
+    "image": "https://images.pexels.com/photos/103662/background-bitter-breakfast-bright-103662.jpeg?cs=srgb&dl=citrus-close-up-food-103662.jpg&fm=jpg&auto=compress&cs=tinysrgb&dpr=2&h=400&w=400",
+    "description": "A telling selection of fruits"
+  },
+  {
+    "name": "Bananas",
+    "price": 16.43,
+    "image": "https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=400&w=400",
+    "description": "A bunch of bananas"
+  },
+  {
+    "name": "Eat healthy",
+    "price": 16.43,
+    "image": "https://images.pexels.com/photos/142520/pexels-photo-142520.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=400&w=400",
+    "description": "Corn, pepper, tomatoes and peas"
+  },
+  {
+    "name": "Tomatoes sprite",
+    "price": 16.43,
+    "image": "https://images.pexels.com/photos/533288/pexels-photo-533288.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=400&w=400",
+    "description": "A Tomato goes with any dish"
+  }
+]
