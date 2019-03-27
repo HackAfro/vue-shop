c1dd8855c32a7aac212392af1a5e837a51789851
Richard Afro <richyafro@hotmail.com>
2019-03-27 22:26:33 +0100

    Store file. Define mutations and actions

--- src/store.js
+++ src/store.js
@@ -1,16 +1,35 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    items: [],
+    cart: [],
   },
   mutations: {
-
+    loadSuccess(state, payload = []) {
+      state.items = [...state.items, ...payload];
+    },
+    addToCart(state, item = {}) {
+      state.cart = [...state.cart, item];
+    },
+    removeFromCart(state, item = {}) {
+      state.cart = [
+        ...state.cart.filter((product) => product.name !== item.name),
+      ];
+    },
   },
   actions: {
-
-  }
-})
+    loadItems({ commit }, items) {
+      commit('loadSuccess', items);
+    },
+    addToCart({ commit }, item) {
+      commit('addToCart', item);
+    },
+    removeFromCart({ commit }, item) {
+      commit('removeFromCart', item);
+    },
+  },
+});
